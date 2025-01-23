7b3eb5ecafc653fd7015f88e47d64085b3d9752d
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2025-01-23 18:34:27 +0600

    added amenities

--- src/components/dashboard/addProperty/AddProperty.js
+++ src/components/dashboard/addProperty/AddProperty.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import FileUpload from './fileUpload/FileUpload'
 import Information from './Information'
+import AmenitiesCheckboxes from '../../home/header/advanceSrch/AmenitiesCheckboxes'
 
 export default function AddProperty() {
+
+
+
+
     return (
         <div className='flex flex-col gap-6'>
 
@@ -19,6 +24,14 @@ export default function AddProperty() {
                 <p className='text-3xl font-semibold mb-3'>Information</p>
 
                 <Information />
+
+            </div>
+
+            <div className='p-6 bg-white rounded-md shadow-xl mb-5'>
+
+                <p className='text-3xl font-semibold mb-4'>Amenities*</p>
+
+                <AmenitiesCheckboxes />
 
             </div>
 
